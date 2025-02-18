f9c38d08521847b672d2131dff6f3a24f1acbc85
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2025-02-18 23:33:08 +0200

    Add separate script for the docker entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Default values for environment variables
+: "${WHISPER_MODEL_NAME:=distil-large-v3}"
+: "${HOST:=0.0.0.0}"
+: "${PORT:=8000}"
+: "${LANGUAGE:=en}"
+: "${MIN_CHUNK_SIZE:=0.5}"
+
+CMD="python3 whisper_fastapi_online_server.py \
+    --host $HOST \
+    --port $PORT \
+    --model $WHISPER_MODEL_NAME \
+    --language $LANGUAGE \
+    --min-chunk-size $MIN_CHUNK_SIZE"
+
+# Execute the command
+exec $CMD
